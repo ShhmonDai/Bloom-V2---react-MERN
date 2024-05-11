d78501bbd44ba767f4b84914abcc8cc58684e5bf
GitShhmon <shhmonp@gmail.com>
2024-05-10 23:09:31 -0400

    add initial goal page goal and subgoal fetching, formating and filtering

--- client/src/components/Goals.jsx
+++ client/src/components/Goals.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 export default function Goals() {
@@ -8,6 +8,7 @@ export default function Goals() {
 
   const [userGoals, setUserGoals] = useState([]);
   const [userSubGoals, setUserSubGoals] = useState([]);
+
 
 
 
@@ -45,30 +46,56 @@ export default function Goals() {
   return (
     <div className='w-full min-h-screen'>
       
+      {/* Main Container */}
+      <div className=' mx-auto pb-10 my-6 flex flex-col justify-center'> 
       
-      <div className=' mx-auto pb-10 flex flex-col justify-center'> 
+      {/* If user logged in map userGoals */}
       {currentUser ? (
+        
         <>
+        {/* User Goals */}
         {userGoals.map((goal) => (
           
-          <div key={goal._id}>
+          
+          <div key={goal._id} className='group m-4' >
 
-            <div> 
+            {/* Title Div */}
+            <div className='flex flex-row border-2 p-10 max-w-2xl w-screen'> 
               <p>Goal Title: {goal.title}</p>
+              <button className='hidden group-hover:inline'>dropdown</button>
             </div>
 
-                    {userSubGoals.map(subgoal => (
+            {/* Inner Div of Goals */}
+            <div className='hidden group-hover:flex flex-col mx-10 px-2 border-2'>
+
+                    {/* User Subgoals */}
+                    {userSubGoals.map((subgoal) => (
+                      <>
+                      {/* Show Subgoals for current Goal */}
+                      {(subgoal.goalId == goal._id) ? 
                         
-                     
-                      <div key={subgoal._id}>
-            
-                        <div>
-                          <li>Subgoal title: {subgoal.title}</li>
+                      (
+                      <div className='' key={subgoal._id}>
+
+                        <div className='flex flex-row gap-2'>
+                                <span>Subgoal title: {subgoal.title}</span>
+                                <button>Edit</button>
                         </div>
-            
+
                       </div>
-            
+                      )
+                      : 
+                      (
+                        <></>
+                      )}
+
+                    </>    
                     ))}
+            </div>
+
+            {/* Outer Div of Goals */}
+            <div className='flex flex-row border-2 p-4 max-w-2xl w-screen'></div>
+
           </div>
 
         ))}
