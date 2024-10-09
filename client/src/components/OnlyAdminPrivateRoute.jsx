6947d1f9e34f0d529aa2e010c63ffec0c8efac5d
GitShhmon <shhmonp@gmail.com>
2024-10-08 21:30:22 -0400

    added private admin route authentication

--- client/src/components/OnlyAdminPrivateRoute.jsx
+++ client/src/components/OnlyAdminPrivateRoute.jsx
@@ -1,8 +1,41 @@
-import { useSelector } from "react-redux"
-import { Outlet, Navigate } from 'react-router-dom'
+import { useSelector, useDispatch } from "react-redux";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { Outlet, Navigate } from 'react-router-dom';
+import { useEffect } from "react";
 
 export default function OnlyAdminPrivateRoute() {
     const { currentUser } = useSelector((state) => state.user)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        const authenticateToken = async () => {
+            try {
+                const res = await fetch('/api/auth/authenticateToken', {
+                    method: 'POST',
+                });
+                const data = await res.json();
+                if (!res.ok) {
+                    dispatch(signoutSuccess());
+
+                }
+                if (res.ok) {
+                    //
+                } else {
+                    console.log(data.message);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        if (currentUser) {
+            authenticateToken();
+        }
+
+    }, [currentUser]);  
+
+
     return currentUser && currentUser.isAdmin ? <Outlet /> : <Navigate to='/sign-in' />
 }
 
