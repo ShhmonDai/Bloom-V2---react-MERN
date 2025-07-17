222e89e278b80ca31f369856e08cdb0d6edc8db4
GitShhmon <shhmonp@gmail.com>
2025-07-17 14:35:10 -0400

    small adjustment to the tiered login rate limit structure

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -14,96 +14,59 @@ const authlimiter = rateLimit({
     },
 });
 
+// 10 tries in 10 minutes
+const stageOne = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 min
+    max: 10, // limit each IP to 10 requests per window
+    headers: false,
+    message: {
+        success: false,
+        statusCode: 429,
+        message: 'Too many login attempts. Please try again in 10 minutes.'
+    },
+});
+
+// 5 more tries in 20 minutes
+const stageTwo = rateLimit({
+    windowMs: 20 * 60 * 1000,
+    max: 10 + 5, 
+    headers: false,
+    message: {
+        success: false,
+        statusCode: 429,
+        message: 'Too many login attempts. Please try again in 20 minutes.'
+    },
+});
+
+// 3 more tries in 1 hour
+const stageThree = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 10 + 5 + 3,
+    headers: false,
+    message: {
+        success: false,
+        statusCode: 429,
+        message: 'Too many login attempts. Please try again in 1 hour'
+    },
+});
+
+// 1 more try in 24 hours
+const stageFour = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 10 + 5 + 3 + 1,
+    headers: false,
+    message: {
+        success: false,
+        statusCode: 429,
+        message: 'Too many login attempts. Please try again in 24 hours'
+    },
+});
+
 const router = express.Router();
 
 router.post('/signup', authlimiter, signup);
-router.post('/signin',
-    // 10 tries in 10 minutes
-    rateLimit({
-        windowMs: 10 * 60 * 1000,
-        headers: false,
-        max: 10,
-        message: {
-            success: false,
-            statusCode: 429,
-            message: 'Too many login attempts. Please try again in 10 minutes.'
-        },
-    }),
-
-    // 5 more tries in 20 minutes
-    rateLimit({
-        windowMs: 20 * 60 * 1000,
-        headers: false,
-        max: 10 + 5,
-        message: {
-            success: false,
-            statusCode: 429,
-            message: 'Too many login attempts. Please try again in 20 minutes.'
-        },
-    }),
-
-    // 3 more tries in 1 hour
-    rateLimit({
-        windowMs: 60 * 60 * 1000,
-        headers: false,
-        max: 10 + 5 + 3,
-        message: {
-            success: false,
-            statusCode: 429,
-            message: 'Too many login attempts. Please try again in 1 hour.'
-        },
-    }),
-
-    // 1 more try in 24 hours
-    rateLimit({
-        windowMs: 24 * 60 * 60 * 1000,
-        headers: false,
-        max: 10 + 5 + 3 + 1,
-        message: {
-            success: false,
-            statusCode: 429,
-            message: 'Too many login attempts. Please try again in 24 hours.'
-        },
-    }),
-     signin
-);
-
-router.post('/signinwallpaper',
-    // 10 tries in 10 minutes
-    rateLimit({
-        windowMs: 10 * 60 * 1000,
-        headers: false,
-        max: 10,
-        message: 'Too many login attempts. Please try again in 10 minutes.',
-    }),
-
-    // 5 more tries in 20 minutes
-    rateLimit({
-        windowMs: 20 * 60 * 1000,
-        headers: false,
-        max: 10 + 5,
-        message: 'Too many login attempts. Please try again in 20 minutes.'
-    }),
-
-    // 3 more tries in 1 hour
-    rateLimit({
-        windowMs: 60 * 60 * 1000,
-        headers: false,
-        max: 10 + 5 + 3,
-        message: 'Too many login attempts. Please try again in 1 hour',
-    }),
-
-    // 1 more try in 24 hours
-    rateLimit({
-        windowMs: 24 * 60 * 60 * 1000,
-        headers: false,
-        max: 10 + 5 + 3 + 1,
-        message: 'Too many login attempts. Please try again in 24 hours',
-    }),
-    signinwallpaper
-);
-
-
+router.post('/signin', stageOne, stageTwo, stageThree, stageFour, signin);
+router.post('/signinwallpaper', stageOne, stageTwo, stageThree, stageFour, signinwallpaper);
 router.post('/signindemo', signindemo);
 router.post('/google', google);
 router.post('/authenticateToken', verifyToken, authenticateToken);
